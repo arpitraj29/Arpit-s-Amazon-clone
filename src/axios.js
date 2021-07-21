8dabcf32d891a829ab2e9b5a6b34ee63e080d69e
ARPIT RAJ <66410585+arpitraj29@users.noreply.github.com>
2021-07-21 22:49:06 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/challenge-ar/us-central1/api'
+  baseURL: "http://localhost:5001/challenge-ar/us-central1/api"
+  //'http://localhost:5001/challenge-ar/us-central1/api'
 });
 
 export default instance;
